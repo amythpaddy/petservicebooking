3169a0984e35657fa281d669f0aec68da65c86f5
Vivek Kumar <vivekgari@gmail.com>
2021-09-11 03:33:59 +0530

    App - Payment integration changes

--- lib/funnels/model/paymentPostData.dart
+++ lib/funnels/model/paymentPostData.dart
@@ -1,9 +1,9 @@
-String url = 'https://plutorum:plutoru*\$*\$@plutorum.com/paytm_payment';
+String url = 'https://plutorum:plutoru*\$*\$@plutorum.com/mobile_paytm_payment';
 String htmlPostData(leadId, leadType) =>
     '<html>' +
     '<body onload="document.f.submit();">' +
     '<form id="f" name="f" method="post" action="$url">' +
-    '<input type="hidden" name="LEAD-ID" value="$leadId">' +
+    '<input type="hidden" name="LEAD_ID" value="$leadId">' +
     '<input type="hidden" name="LEAD_TYPE" value="$leadType">' +
     '</form>' +
     '</body>' +
